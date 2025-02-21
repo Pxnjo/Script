0c40929deb2028654c7f80cde5bf80e35328ad84
Pxnjo <alecustu7@gmail.com>
2025-02-21 17:03:56 +0100

    clonazione e resize del disco funzionante

--- proxmox-script.py
+++ proxmox-script.py
@@ -1,33 +1,47 @@
 import requests
-from proxmoxer import ProxmoxAPI
 
-proxmox = ProxmoxAPI('10.20.82.250', user='root@pam', password='foobar', verify_ssl=False)
-print(proxmox)
+node = "pve-m01"
+# API Token
+headers = {
+    'Authorization': 'PVEAPIToken=root@pam!pxnjoToken=47e7e567-d637-4146-b097-f78fbdd14c7d',
+    'Content-Type': 'application/json'
+}
+
+# Clone VM from template
+# Get already used VM id
+url = f"https://10.20.82.250:8006/api2/json/nodes/{node}/qemu"
+response = requests.get(url, headers=headers, verify=False)
+if response.status_code == 200:
+    vm_list = response.json()['data']
+    vm_ids = [vm['vmid'] for vm in vm_list if vm['vmid'] < 10000]
+    #print("VMIDs:", vm_ids)
+else:
+    print(f"Error {response.status_code}: {response.text}")
+newid = 100
+while newid in vm_ids:  # Se newid è già nella lista, incrementalo
+    newid += 1
+# Clone VM
+templateId = (input("Enter the template VM ID: "))
+url = f"https://10.20.82.250:8006/api2/json/nodes/{node}/qemu/{templateId}/clone"
+data = {
+    "newid": newid
+}
+response = requests.post(url, headers=headers, json=data, verify=False)
+if response.status_code == 200:
+    print("VM cloned successfully")
+else:
+    print("VM clone failed")
+    print(response.text)
 
 
-
-
-
-
-
-
-
-
-
-
-
-# url = "https://10.20.82.250:8006/api2/json/nodes/pve-m01/qemu/100/status/start"
-# headers = {
-#     'Authorization': 'PVEAPIToken=root@pam!testVM=3738178e-0d67-495c-ac89-e9eb3eff565b',
-#     "Content-Type": "application/json"
-# }
-# response = requests.post(url, headers=headers, verify=False)
-
-# if response.status_code == 200:
-#     print("VM started successfully")
-# else:
-#     print(response)
-#     print("Failed to start VM")
-
-
-
+#Resize disk
+data = {
+"disk": "scsi0",
+"size": "+10G"
+}
+url = f"https://10.20.82.250:8006/api2/json/nodes/{node}/qemu/{vmid}/resize"
+response = requests.put(url, headers=headers, json=data, verify=False)
+if response.status_code == 200:
+    print("Disk resized successfully")
+else:
+    print("Disk resize failed")
