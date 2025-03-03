c7ce80d39cd047edb8f010e4f8f2c3f58d6117cd
Pxnjo <alecustu7@gmail.com>
2025-03-03 11:43:35 +0100

    finito script

--- main_project/config.py
+++ main_project/config.py
@@ -1,12 +1,12 @@
 # proxmox server
 node = "pve"
-server = "192.168.144.128"
-gateway = "192.168.144.2" # gateway delle VM
-hostname = "rackone.prova"
+server = "10.20.30.40"
+gateway = "10.20.30.40" # gateway delle VM
+hostname = "test.prova"
 private_key_path = "main_project/id_ed25519"
 public_key_path = "main_project/id_ed25519.pub"
 # API Token !!Manage Carefully!!
-API_TOKEN = 'PVEAPIToken=root@pam!testCasa=1a3ef4e0-412e-405c-be2e-b495f6320b84'
+API_TOKEN = 'PVEAPIToken='
 headers = {
     'Authorization': API_TOKEN,
     'Content-Type': 'application/json'
