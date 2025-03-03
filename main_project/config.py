795f5a44563c6750df5eb2ed96d9057cfcfcfb6a
Pxnjo <alecustu7@gmail.com>
2025-03-03 17:01:48 +0100

    ignorato il file config

--- main_project/config.py
+++ main_project/config.py
@@ -1,12 +1,12 @@
 # proxmox server
-node = "pve-m01"
-server = "10.20.82.250"
-hostname = "rackone.prova"
-gateway = "10.0.1.100"
+node = "pve"
+server = "10.20.30.40"
+gateway = "10.20.30.40" # gateway delle VM
+hostname = "test.prova"
 private_key_path = "main_project/id_ed25519"
 public_key_path = "main_project/id_ed25519.pub"
 # API Token !!Manage Carefully!!
-API_TOKEN = 'PVEAPIToken=root@pam!pxnjoToken=47e7e567-d637-4146-b097-f78fbdd14c7d'
+API_TOKEN = 'PVEAPIToken='
 headers = {
     'Authorization': API_TOKEN,
     'Content-Type': 'application/json'
