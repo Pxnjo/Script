312fa9d1fc7372a5a0dbf9c81eb2d76f51b2299a
Pxnjo <alecustu7@gmail.com>
2025-03-03 17:55:56 +0100

    cominciate prove per la web_app

--- web_app/backend.py
+++ web_app/backend.py
@@ -2,3 +2,11 @@ from flask import Flask, jsonify, request
 from main_project import script
 
 app = Flask(__name__)
+
+@app.route('/api', methods=['GET'])
+def call_script():
+    result = script.tua_funzione()
+    return jsonify(result)
+
+if __name__ == '__main__':
+    app.run(debug=True)
